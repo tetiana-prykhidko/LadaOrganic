a8e3efcf7875f23ebb8ac15ae94a3b15ef33fa0d
Tetiana <tetiana_prykhidko@ukr.net>
2020-05-18 15:22:21 +0300

    add preloader

--- js/main.js
+++ js/main.js
@@ -32,12 +32,24 @@ $(function() {
 
 		$("html, body").animate({
 			scrollTop: blockOffset
-		}, 1000);
-
-
-		
+		}, 1000);	
 	});
+});
 
-
+// Scroll back to top
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back-to-top').fadeIn();
+    } else {
+        $('#back-to-top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back-to-top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
 
 });
